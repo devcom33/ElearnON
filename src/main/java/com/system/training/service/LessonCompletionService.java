9e7fa4524aaa19750f566c8ca26449786d49203a
devcom33 <mouad.elb06@gmail.com>
2024-09-08 22:37:38 +0100

    add markLessonAsComplete method

--- src/main/java/com/system/training/service/LessonCompletionService.java
+++ src/main/java/com/system/training/service/LessonCompletionService.java
@@ -3,6 +3,11 @@ package com.system.training.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.system.training.exception.LessonNotFoundException;
+import com.system.training.exception.StudentNotFoundException;
+import com.system.training.model.Lesson;
+import com.system.training.model.LessonCompletion;
+import com.system.training.model.Student;
 import com.system.training.repository.LessonCompletionRepository;
 
 
@@ -10,9 +15,27 @@ import com.system.training.repository.LessonCompletionRepository;
 public class LessonCompletionService {
 	
 	@Autowired
-	public LessonCompletionRepository lcService;
+	public LessonCompletionRepository lcRepository;
+	public LessonService lessonService;
+	public StudentService studentService;
 
 	public Long countCompletedLessons(Long courseId, Long studentId){
-		return lcService.countbyCompleted(courseId, studentId);
+		return lcRepository.countbyCompleted(courseId, studentId);
+	}
+	
+	/*
+	 * markLessonAsComplete - A function To mark the lesson as completed
+	 */
+	public void markLessonAsComplete(Long studentId, Long lessonId) throws LessonNotFoundException, StudentNotFoundException {
+		LessonCompletion lessonCompletion = new LessonCompletion();
+		
+		Lesson lesson = lessonService.getLessonById(lessonId);
+		Student student = studentService.getStudentById(lessonId);
+		
+		lessonCompletion.setLesson(lesson);
+		lessonCompletion.setStudent(student);
+		lessonCompletion.setCompleted(true);
+		
+		lcRepository.save(lessonCompletion);
 	}
 }
