dcc32f94c5b0fa3e57adc0408a2f75dbdf3dc1f5
devcom33 <mouad.elb06@gmail.com>
2024-09-13 22:40:05 +0100

    fix some issues i encountered in jwt authentification

--- src/main/java/com/system/training/repository/AppRoleRepository.java
+++ src/main/java/com/system/training/repository/AppRoleRepository.java
@@ -3,11 +3,12 @@ package com.system.training.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.system.training.enums.RoleName;
 import com.system.training.model.AppRole;
 
 
 
 @Repository
 public interface AppRoleRepository extends JpaRepository<AppRole, Long>{
-	AppRole findByName(AppRole name);
+	AppRole findByName(RoleName name);
 }
--- src/main/java/com/system/training/service/UserService.java
+++ src/main/java/com/system/training/service/UserService.java
@@ -1,14 +1,18 @@
 package com.system.training.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.system.training.controller.EnrollmentController;
 import com.system.training.model.AppRole;
 import com.system.training.model.AppUser;
 import com.system.training.repository.AppRoleRepository;
@@ -16,6 +20,7 @@ import com.system.training.repository.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService{
+	private static final Logger logger = LoggerFactory.getLogger(EnrollmentController.class);
 	@Autowired
 	public UserRepository userRepository;
 	@Autowired
@@ -24,6 +29,26 @@ public class UserService implements UserDetailsService{
     public PasswordEncoder passwordEncoder;	
 	
 	public AppUser createUser(AppUser appUser) {
+		logger.info("AppUser "+ appUser.getRoles());
+		Set<AppRole> roles = appUser.getRoles();
+		AppRole role = new AppRole();
+		Set<AppRole> newRoles = new HashSet<AppRole>();
+		
+		for (AppRole roleElement: roles) {
+			role.setId(roleElement.getId());
+			role.setName(roleElement.getName());
+			logger.error("complete[-1]");
+			if (appRoleRepository.findByName(role.getName()) != null) {
+				logger.error("complete[0]");
+				newRoles.add(role);
+				logger.error("complete[1]");
+
+			}
+			
+			logger.error("complete[2]");
+		}
+		logger.info(role.getAuthority());
+		appUser.setRoles(newRoles);
 		appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
 		return userRepository.save(appUser);
 	}
@@ -31,13 +56,13 @@ public class UserService implements UserDetailsService{
 	public AppUser findUserByUsername(String username) {
 		return userRepository.findByUsername(username);
 	}
-	
+	/*
 	public void assignRoleToUser(String username, AppRole appRole) {
 		AppUser user = findUserByUsername(username);
 		AppRole role = appRoleRepository.findByName(appRole);
 		user.getRoles().add(role);
 		userRepository.save(user);
-	}
+	}*/
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
